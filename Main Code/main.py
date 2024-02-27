db67f877e5fdbc1d9656e77a41c7854194307469
Sarfaraz Ahmed <20cs067@students.muet.edu.pk>
2024-02-27 19:40:08 +0500

    upadted

--- Main Code/main.py
+++ Main Code/main.py
@@ -4,19 +4,30 @@ import os
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
 
 load_dotenv('var.env')
 key = os.getenv('OPENAI_API_KEY')
 # initialize connection to pinecone (get API key at app.pinecone.io)
-api_key = '06adbd3d-ae02-4246-ac25-2039f86cec55'
+api_key = ''
 
 # configure client
 pc = Pinecone(api_key=api_key)
 
 
-loader = TextLoader("../../modules/state_of_the_union.txt")
+loader = TextLoader("Main Code\Data\ahmed.txt")
 documents = loader.load()
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 docs = text_splitter.split_documents(documents)
 
 embeddings = OpenAIEmbeddings()
+
+
+index_name = "langchain-test-index"
+
+docsearch = PineconeVectorStore.from_documents(docs, embeddings, index_name=index_name)
+
+query = "What did the president say about Ketanji Brown Jackson"
+docs = docsearch.similarity_search(query)
+print(docs[0].page_content)

283fe9049ba74e9756c17dace4db59f5d2dc5211
Sarfaraz Ahmed <20cs067@students.muet.edu.pk>
2024-03-19 06:18:46 +0500

    updation in code

---
+++ Main Code/helpers.py
@@ -0,0 +1,5 @@
+from rag import chain
+
+
+def chat_completion(prompt):
+    return chain.chat_completion(prompt)
--- Main Code/main.py
+++ Main Code/main.py
@@ -1,33 +1,20 @@
-from pinecone import Pinecone
-from dotenv import load_dotenv
-import os
-from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.document_loaders import TextLoader
-from langchain_openai import OpenAIEmbeddings
-from langchain_pinecone import PineconeVectorStore
+# main.py
+from fastapi import FastAPI
+from rag import RAG, initialize_rag
+from helpers import chat_completion
+
+app = FastAPI()
+
+rag = RAG()
 
 
-load_dotenv('var.env')
-key = os.getenv('OPENAI_API_KEY')
-# initialize connection to pinecone (get API key at app.pinecone.io)
-api_key = ''
-
-# configure client
-pc = Pinecone(api_key=api_key)
+@app.post("/finetune/")
+async def finetune(directory_path: str):
+    initialize_rag(rag, directory_path)  # Passing rag object
+    return {"message": "Finetuning done Successfully"}
 
 
-loader = TextLoader("Main Code\Data\ahmed.txt")
-documents = loader.load()
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-docs = text_splitter.split_documents(documents)
-
-embeddings = OpenAIEmbeddings()
-
-
-index_name = "langchain-test-index"
-
-docsearch = PineconeVectorStore.from_documents(docs, embeddings, index_name=index_name)
-
-query = "What did the president say about Ketanji Brown Jackson"
-docs = docsearch.similarity_search(query)
-print(docs[0].page_content)
+@app.post("/chat/")
+async def chat(prompt: str):
+    response = chat_completion(prompt)
+    return {"response": response}

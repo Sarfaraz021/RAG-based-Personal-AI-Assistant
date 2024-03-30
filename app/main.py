45960509296ee047034102017fbd310a49a914e9
Sarfaraz Ahmed <20cs067@students.muet.edu.pk>
2024-03-30 12:30:53 +0500

    up

--- app/main.py
+++ app/main.py
@@ -12,12 +12,12 @@ app = FastAPI(title="RAG Assistant API",
 
 
 # @app.on_event("startup")
-# async def startup_db_client():
+# async def startup_event():
 #     await connect_to_mongo()
 
 
 # @app.on_event("shutdown")
-# async def shutdown_db_client():
+# async def shutdown_event():
 #     await close_mongo_connection()
 
 app.add_middleware(
